c28e3aa76fdd4f45e383d92556fbc130519382a4
y-magavel <magavel.dev@gmail.com>
2022-05-28 03:22:03 +0900

    Headerコンポーネントを追加
    ヘッダー部分のコンポーネントを実装

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {AppBar, Box, Button, styled, Toolbar, Typography} from "@mui/material";
+
+const CustomToolbar = styled(Toolbar)({
+    justifyContent: 'space-between',
+}) as typeof Toolbar;
+
+const CustomTitle = styled(Typography)({
+    fontWeight: 700,
+    letterSpacing: '.3rem',
+    color: 'inherit',
+    cursor: 'pointer',
+}) as typeof Typography;
+
+export const Header: React.FC = () => {
+    return (
+        <>
+            <AppBar position="static">
+                <CustomToolbar>
+                    <CustomTitle component="a">サブスク管理アプリ</CustomTitle>
+
+                    <Box>
+                        <Button variant="outlined" color="inherit">ログイン</Button>
+                    </Box>
+
+                </CustomToolbar>
+            </AppBar>
+        </>
+    );
+};
