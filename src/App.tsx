f675e7543a7a977d9c430e1d71ffaf24d9614170
y-magavel <magavel.dev@gmail.com>
2022-05-28 18:34:15 +0900

    SignUpコンポーネントを追加
    ユーザー新規登録画面のコンポーネント

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./components/Header";
+import {SignUp} from "./components/SignUp";
 
 function App() {
     return (
         <>
             <Header/>
+            <SignUp/>
         </>
     );
 }
---
+++ src/components/SignUp.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {Button, Card, CardContent, Container, Stack, TextField, Typography} from "@mui/material";
+
+export const SignUp: React.FC = () => {
+    return (
+        <>
+            <Container sx={{height: '100vh',display: 'flex', alignItems: 'center'}}>
+                <Card sx={{width: '30%', minWidth: 325, maxWidth: 500, margin: 'auto'}}>
+                    <CardContent>
+                        <Stack spacing={2}>
+                            <Typography sx={{textAlign: 'center', fontWeight: 'bold',}}>ユーザー新規登録</Typography>
+                            <TextField required id="outlined-required" label="メールアドレス"/>
+                            <TextField required id="outlined-password-input" label="パスワード" type="password"
+                                       autoComplete="current-password"/>
+                            <Button variant="contained">登録する</Button>
+                        </Stack>
+                    </CardContent>
+                </Card>
+            </Container>
+        </>
+    );
+};
