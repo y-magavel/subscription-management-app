22043f00d6ab63d81954efe6b77d053d983d0705
y-magavel <magavel.dev@gmail.com>
2022-05-29 22:40:25 +0900

    componentsディレクトリにpagesディレクトリを作成
    Login.tsxとSignUp.tsxをpagesディレクトリに移動

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Route, Routes} from "react-router-dom";
-import {SignUp} from "../components/SignUp";
-import {Login} from "../components/Login";
+import {SignUp} from "../components/pages/SignUp";
+import {Login} from "../components/pages/Login";
 
 export const Router: React.FC = () => {
     return (
