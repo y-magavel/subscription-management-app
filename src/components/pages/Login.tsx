ab9a0299bcde13ec72cac28791ee63c3705d6abd
y-magavel <magavel.dev@gmail.com>
2022-06-01 02:01:19 +0900

    ログイン機能を実装
    作りはユーザー新規登録機能とほぼ同じ

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,20 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import {Button, Card, CardContent, Container, Stack, TextField, Typography} from "@mui/material";
 import {Header} from "../organisms/Header";
+import {logIn} from "../../services/api";
 
 export const Login: React.FC = () => {
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    // メールアドレスを入力したとき
+    const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value);
+    };
+
+    // パスワードを入力したとき
+    const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    };
+
+    // ログインボタンを押したとき
+    const onClickLogin = async (email: string, password: string) => {
+        console.log(email);
+        console.log(password);
+        await logIn(email, password);
+        setEmail("");
+        setPassword("");
+    };
+
     return (
         <>
             <Header/>
-            <Container sx={{height: '100vh',display: 'flex', alignItems: 'center'}}>
+            <Container sx={{height: '100vh', display: 'flex', alignItems: 'center'}}>
                 <Card sx={{width: '30%', minWidth: 325, maxWidth: 500, margin: 'auto'}}>
                     <CardContent>
                         <Stack spacing={2}>
                             <Typography sx={{textAlign: 'center', fontWeight: 'bold',}}>ログイン</Typography>
-                            <TextField required id="outlined-required" label="メールアドレス"/>
+                            <TextField required id="outlined-required" label="メールアドレス" value={email}
+                                       onChange={onChangeEmail}/>
                             <TextField required id="outlined-password-input" label="パスワード" type="password"
-                                       autoComplete="current-password"/>
-                            <Button variant="contained">ログインする</Button>
+                                       autoComplete="current-password" value={password} onChange={onChangePassword}/>
+                            <Button variant="contained" onClick={() => onClickLogin(email, password)}>ログインする</Button>
                         </Stack>
                     </CardContent>
                 </Card>
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,16 +1,32 @@
-import {getAuth, createUserWithEmailAndPassword} from "firebase/auth";
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
 
+// ユーザー新規登録
 export const signUp = (email: string, password: string) => {
     const auth = getAuth();
+
     createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-            // Signed in
-            // const user = userCredential.user;
-            console.log(userCredential.user);
+            const user = userCredential.user;
+            console.log(`登録成功: ${user.uid}`);
         })
         .catch((error) => {
-            // const errorCode = error.code;
-            // const errorMessage = error.message;
-            console.log(error.message);
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(`${errorCode}: ${errorMessage}`);
+        });
+};
+
+// ログイン
+export const logIn = (email: string, password: string) => {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log(`ログイン成功: ${user.uid}`);
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(`${errorCode}: ${errorMessage}`);
         });
 };
