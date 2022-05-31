aea176b280eb0bd56bc01dbdf6fe73d6f317d219
y-magavel <magavel.dev@gmail.com>
2022-06-01 03:02:27 +0900

    ログイン状態に応じてヘッダーの表示切り替えを実装
    ログイン中はログアウトボタンのみ、ログアウト中は新規登録ボタンとログインボタンを表示する

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import {AppBar, Box, Button, styled, Toolbar, Typography} from "@mui/material";
 import {useNavigate} from "react-router-dom";
 import {logOut} from "../../services/api";
+import {useRecoilValue} from "recoil";
+import {authState} from "../../store/auth";
 
 const CustomToolbar = styled(Toolbar)({
     justifyContent: 'space-between',
@@ -16,26 +18,38 @@ const CustomTitle = styled(Typography)({
 
 export const Header: React.FC = () => {
     const navigate = useNavigate();
+    // ログインしていたらtrue、していなければfalse
+    const isLogined = useRecoilValue(authState) !== "";
 
     // ログアウトボタンを押したとき
-    const onClickLogout = () => {
-        logOut();
+    const onClickLogout = async () => {
+        await logOut();
     };
 
     return (
         // <>
-            <AppBar position="fixed">
-                <CustomToolbar>
-                    <CustomTitle component="a" onClick={() => navigate("/")}>サブスク管理アプリ</CustomTitle>
+        <AppBar position="fixed">
+            <CustomToolbar>
+                <CustomTitle component="a" onClick={() => navigate("/")}>サブスク管理アプリ</CustomTitle>
 
-                    <Box>
-                        <Button variant="outlined" color="inherit" onClick={() => navigate("/signup")}>新規登録</Button>
-                        <Button variant="outlined" color="inherit" onClick={() => navigate("/login")}>ログイン</Button>
-                        <Button variant="outlined" color="inherit" onClick={onClickLogout}>ログアウト</Button>
-                    </Box>
+                <Box>
+                    {isLogined ? (
+                        // ログインしていたら、こっちを表示する
+                        <>
+                            <Button variant="outlined" color="inherit" onClick={onClickLogout}>ログアウト</Button>
+                        </>
+                    ) : (
+                        // ログインしていなければ、こっちを表示する
+                        <>
+                            <Button variant="outlined" color="inherit" onClick={() => navigate("/signup")}>新規登録</Button>
+                            <Button variant="outlined" color="inherit" onClick={() => navigate("/login")}>ログイン</Button>
+                        </>
 
-                </CustomToolbar>
-            </AppBar>
+                    )}
+                </Box>
+
+            </CustomToolbar>
+        </AppBar>
         // </>
     );
 };
--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,19 +1,19 @@
 import {atom} from "recoil";
 import {getAuth, onAuthStateChanged} from "firebase/auth";
 
-console.log('store');
-const auth = getAuth();
-
 // ログイン中のユーザーのuidを保持する
 export const authState = atom({
     key: "authState",
     default: "",
     effects: [
         ({setSelf}) => {
+            const auth = getAuth();
             onAuthStateChanged(auth, (user) => {
                 if (user) {
                     console.log(user.uid);
                     setSelf(user.uid);
+                } else {
+                    setSelf("");
                 }
             });
         },
