d09e6f5cb4cee4421e838e5e91e2a3f55c351378
y-magavel <magavel.dev@gmail.com>
2022-06-11 17:52:21 +0900

    サブスク一覧の表示処理を実装
    サブスク一覧配列をmapで回してひとつずつカード要素にして生成

--- src/components/organisms/ServiceList.tsx
+++ src/components/organisms/ServiceList.tsx
@@ -1,35 +1,42 @@
-import React from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import {Card, CardActionArea, CardContent, Stack, Typography} from "@mui/material";
+import {getServiceList} from "../../services/api";
+import {useAuthWithUid} from "../../store/auth";
+import {Service} from "../../types/service";
 
 export const ServiceList: React.FC = () => {
+    const loginUser = useAuthWithUid();
+    const [serviceList, setServiceList] = useState<Array<Service>>([]);
+
+    // 一覧データ取得/更新
+    const fetch = useCallback(async () => {
+        const data = await getServiceList(loginUser);
+        setServiceList(data);
+    }, [loginUser]);
+
+    // 初回画面表示時
+    useEffect( () => {
+        (async () => {
+            await fetch();
+        })();
+    }, [fetch]);
+
     return (
         <>
             <Stack spacing={2} sx={{width: "100%", alignItems: "center"}}>
                 <p>サブスク一覧</p>
-                <Card sx={{width: "70%"}}>
-                    <CardActionArea>
-                        <CardContent>
-                            <Typography>Netflix</Typography>
-                            <Typography>1490円／月</Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
-                <Card sx={{width: "70%"}}>
-                    <CardActionArea>
-                        <CardContent>
-                            <Typography>Apple Music</Typography>
-                            <Typography>1490円／月</Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
-                <Card sx={{width: "70%"}}>
-                    <CardActionArea>
-                        <CardContent>
-                            <Typography>Hulu</Typography>
-                            <Typography>1490円／月</Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
+                {serviceList.map((service) => {
+                    return (
+                        <Card key={service.id} sx={{width: "70%"}}>
+                            <CardActionArea>
+                                <CardContent>
+                                    <Typography>{service.serviceName}</Typography>
+                                    <Typography>{service.servicePrice}／{service.paymentCycle}</Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    );
+                })}
             </Stack>
         </>
     );
