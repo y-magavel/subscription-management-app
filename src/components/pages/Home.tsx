58cdc5acfb4a288c3b659975da02e0816d959454
y-magavel <magavel.dev@gmail.com>
2022-06-11 15:16:34 +0900

    ServiceListコンポーネントを追加
    登録したサブスクをリスト形式で一覧表示する

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -3,6 +3,7 @@ import {Header} from "../organisms/Header";
 import {Container} from "@mui/material";
 import {Footer} from "../organisms/Footer";
 import {RegisterModal} from "../organisms/RegisterModal";
+import {ServiceList} from "../organisms/ServiceList";
 
 export const Home: React.FC = () => {
     // サブスク登録モーダルの開閉用State
@@ -12,7 +13,8 @@ export const Home: React.FC = () => {
         <>
             <Header/>d
             <Container sx={{height: '100vh', display: 'flex', alignItems: 'center'}}>
-                ホーム
+                サブスク一覧
+                <ServiceList/>
             </Container>
             <RegisterModal open={open} setOpen={setOpen}/>
             <Footer open={open} setOpen={setOpen}/>
