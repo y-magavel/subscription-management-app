090f3560141655867508977e74c7e53e2301d46e
y-magavel <magavel.dev@gmail.com>
2022-06-12 17:44:04 +0900

    サブスク一覧の表示処理をHomeコンポーネントに移動
    fetch関数をRegisterModalコンポーネントに渡して、サブスク追加時に一覧を更新したいから

--- src/components/organisms/ServiceList.tsx
+++ src/components/organisms/ServiceList.tsx
@@ -1,37 +1,27 @@
-import React, {useCallback, useEffect, useState} from "react";
+import React from "react";
 import {Card, CardActionArea, CardContent, Stack, Typography} from "@mui/material";
-import {getServiceList} from "../../services/api";
-import {useAuthWithUid} from "../../store/auth";
 import {Service} from "../../types/service";
 
-export const ServiceList: React.FC = () => {
-    const loginUser = useAuthWithUid();
-    const [serviceList, setServiceList] = useState<Array<Service>>([]);
+// 受け取るpropsの型定義
+type Props = {
+    data: Array<Service>;
+};
 
-    // 一覧データ取得/更新
-    const fetch = useCallback(async () => {
-        const data = await getServiceList(loginUser);
-        setServiceList(data);
-    }, [loginUser]);
-
-    // 初回画面表示時
-    useEffect( () => {
-        (async () => {
-            await fetch();
-        })();
-    }, [fetch]);
+export const ServiceList: React.FC<Props> = (props) => {
+    // Propsの受け取り
+    const {data} = props;
 
     return (
         <>
             <Stack spacing={2} sx={{width: "100%", alignItems: "center"}}>
                 <p>サブスク一覧</p>
-                {serviceList.map((service) => {
+                {data.map((service) => {
                     return (
-                        <Card key={service.id} sx={{width: "70%"}}>
+                        <Card key={service.id} sx={{width: {xs: "90%", md: "70%"}}}>
                             <CardActionArea>
                                 <CardContent>
                                     <Typography>{service.serviceName}</Typography>
-                                    <Typography>{service.servicePrice}／{service.paymentCycle}</Typography>
+                                    <Typography>{service.servicePrice}円／{service.paymentCycle}</Typography>
                                 </CardContent>
                             </CardActionArea>
                         </Card>
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,19 +1,38 @@
-import React, {useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import {Header} from "../organisms/Header";
 import {Container} from "@mui/material";
 import {Footer} from "../organisms/Footer";
 import {RegisterModal} from "../organisms/RegisterModal";
 import {ServiceList} from "../organisms/ServiceList";
+import {useAuthWithUid} from "../../store/auth";
+import {Service} from "../../types/service";
+import {getServiceList} from "../../services/api";
 
 export const Home: React.FC = () => {
     // サブスク登録モーダルの開閉用State
     const [open, setOpen] = useState<boolean>(false); // TODO: カスタムフックにすることを検討する
 
+    const loginUser = useAuthWithUid();
+    const [serviceList, setServiceList] = useState<Array<Service>>([]);
+
+    // 一覧データ取得/更新
+    const fetch = useCallback(async () => {
+        const data = await getServiceList(loginUser);
+        setServiceList(data);
+    }, [loginUser]);
+
+    // 初回画面表示時
+    useEffect( () => {
+        (async () => {
+            await fetch();
+        })();
+    }, [fetch]);
+
     return (
         <>
             <Header/>
             <Container sx={{height: '100vh', display: 'flex', alignItems: 'center'}}>
-                <ServiceList/>
+                <ServiceList data={serviceList}/>
             </Container>
             <RegisterModal open={open} setOpen={setOpen}/>
             <Footer open={open} setOpen={setOpen}/>
