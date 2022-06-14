b14a6ea82201f13d72b4c2173a7bd0bd7b58e0af
y-magavel <magavel.dev@gmail.com>
2022-06-14 23:16:18 +0900

    一覧画面のページネーションを実装
    1ページあたり10個ずつサブスクを表示する

--- src/components/organisms/ServiceList.tsx
+++ src/components/organisms/ServiceList.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import {Card, CardActionArea, CardContent, Stack, Typography} from "@mui/material";
+import React, {useEffect, useState} from "react";
+import {Card, CardActionArea, CardContent, Pagination, Stack, Typography} from "@mui/material";
 import {Service} from "../../types/service";
 
 // 受け取るpropsの型定義
@@ -12,11 +12,34 @@ export const ServiceList: React.FC<Props> = (props) => {
     // Propsの受け取り
     const {data, openServiceDetail} = props;
 
+    // Stateの宣言
+    const [page, setPage] = useState<number>(1);
+    const [displayData, setDisplayData] = useState<Array<Service>>([]);
+
+    // 1ページあたりのサブスク表示上限
+    const displayLimit: number = 10;
+
+    // ページ毎に表示するサブスクを詰め替える
+    useEffect(() => {
+        let selectedData: Array<Service> = [];
+        data.forEach((service, index) => {
+            if ((index >= ((page - 1) * displayLimit)) && (index < (page * displayLimit))) {
+                selectedData.push(service);
+            }
+        });
+        setDisplayData(selectedData);
+    }, [page, data]);
+
+    // ページネーションでページを切り替えたら
+    const onChangePagination = (e: React.ChangeEvent<unknown>, value: number) => {
+        setPage(value);
+    };
+
     return (
         <>
             <Stack spacing={2} sx={{width: "100%", alignItems: "center"}}>
                 <p>サブスク一覧</p>
-                {data.map((service) => {
+                {displayData.map((service) => {
                     return (
                         <Card key={service.id} sx={{width: {xs: "90%", md: "70%"}}}>
                             <CardActionArea
@@ -29,6 +52,7 @@ export const ServiceList: React.FC<Props> = (props) => {
                         </Card>
                     );
                 })}
+                <Pagination count={Math.ceil(data.length / displayLimit)} color="primary" page={page} onChange={onChangePagination} sx={{paddingBottom: '100px'}} />
             </Stack>
         </>
     );
