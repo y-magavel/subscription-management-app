321f690cd6d94068601324e88447b234af9b5349
y-magavel <magavel.dev@gmail.com>
2022-06-17 00:53:09 +0900

    404ページを作成
    画像とトップページに戻るボタンを設置

--- src/components/pages/Page404.tsx
+++ src/components/pages/Page404.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import {Header} from "../organisms/Header";
-import {Container} from "@mui/material";
+import {Button, Container, Typography} from "@mui/material";
+import page404Blue from "../../images/page404-blue.png";
 
 export const Page404: React.FC = () => {
     return (
         <>
             <Header/>
-            <Container sx={{height: '100vh',display: 'flex', alignItems: 'center'}}>
-                <p>404 Not Found.</p>
+            <Container sx={{display: 'flex', flexFlow: 'column', alignItems: 'center', paddingY: {xs: '56px', md: '64px'}}}>
+                <Typography variant="h2" sx={{marginTop: "100px"}}>
+                    404
+                </Typography>
+                <Typography>
+                    ページが見つかりませんでした。
+                </Typography>
+                <img src={page404Blue} alt="段ボールに隠れている人" width={"60%"}/>
+                <Button variant="contained" href="/">トップページに戻る</Button>
             </Container>
         </>
     );
