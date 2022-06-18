899572c502ef9794e5fd4e17d0bcd0099e6ac072
y-magavel <magavel.dev@gmail.com>
2022-06-19 02:39:40 +0900

    メール検証状況に応じてルーティングするよう変更
    PrivateRoutesの中にあるルート同士でリダイレクトすると無限ループが起きるので、それを防ぐために冗長な書き方になっている。

--- src/router/PrivateRoutes.tsx
+++ src/router/PrivateRoutes.tsx
@@ -1,14 +1,39 @@
 import React from "react";
 import {useLocation, Outlet, Navigate} from "react-router-dom";
-import {useAuth} from "../store/auth";
+import {useAuth, getAuthEmail} from "../store/auth";
 
 export const PrivateRoutes: React.FC = () => {
     const location = useLocation();
     console.log(location);
     console.log(`リダイレクト元：${location.pathname}`);
 
+    // ログインしているか
     const isLogined = useAuth();
-    console.log(`ログインしているか: ${isLogined}`);
+    // メールアドレスが検証済みか
+    const isVerifiedEmail = getAuthEmail();
 
-    return isLogined ? <Outlet/> : <Navigate to="/login" replace state={{from: location}}/>;
+    // 遷移先を決める
+    let target = isLogined ? <Outlet/> : <Navigate to="/login" replace state={{from: location}}/>;
+
+
+    // TODO: PrivateRoutesの中にあるルート同士でリダイレクトすると無限ループが起きるので、それを防ぐための条件分岐。冗長なのでリファクタリングする。
+    // ホームを指定してアクセスしてきた場合はこっち
+    if (isLogined && location.pathname.includes("home")) {
+        if (isVerifiedEmail) {
+            target = <Outlet/>;
+        } else {
+            target = <Navigate to="/wait-email-verify" replace/>;
+        }
+    }
+
+    // メール確認待ち画面を直接指定してアクセスしてきた場合はこっち
+    if (isLogined && location.pathname.includes("wait-email-verify")) {
+        if (isVerifiedEmail) {
+            target = <Navigate to="/home" replace/>;
+        } else {
+            target = <Outlet/>;
+        }
+    }
+
+    return target;
 };
