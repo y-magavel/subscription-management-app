b40fe3e908dc4452a0ab74e9601215ab8f9be830
y-magavel <magavel.dev@gmail.com>
2022-06-22 01:13:34 +0900

    パスワード再設定の画面側の処理を実装
    正しい形式のメールアドレスが入力されたら送信ボタンを有効化する

--- src/components/pages/ForgotPassword.tsx
+++ src/components/pages/ForgotPassword.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import {Header} from "../organisms/Header";
 import {AlertArea} from "../organisms/AlertArea";
 import {Button, Card, CardContent, Container, Stack, TextField, Typography} from "@mui/material";
+import {useSetCustomAlert} from "../../store/alert";
 
 export const ForgotPassword: React.FC = () => {
+
+    const [email, setEmail] = useState<string>("");
+    const [sendEmailButtonDisabled, setSendEmailButtonDisabled] = useState<boolean>(true); // 送信ボタンの有効/無効化切り替え用
+    let pattern: RegExp = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]+.[A-Za-z0-9]+$/; // メールアドレスの正規表現
+
+    const setCustomAlert = useSetCustomAlert();
+
+    // メールアドレスが入力されたら
+    const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value);
+        setSendEmailButtonDisabled(!pattern.test(e.target.value)); // 正しい形式のメールアドレスが入力されたら送信ボタンを有効化
+    };
+
+    // 再設定メール送信ボタンがクリックされたら
+    const onClickSendEmailButton = () => {
+        // TODO: 再設定メール送信処理
+        setCustomAlert({open: true, message: `宛先：${email}にパスワード再設定メールを送信しました。`, type: "success"});
+        setEmail("");
+    };
+
     return (
         <>
             <Header/>
@@ -13,9 +34,10 @@ export const ForgotPassword: React.FC = () => {
                     <CardContent>
                         <Stack spacing={2}>
                             <Typography sx={{textAlign: 'center', fontWeight: 'bold',}}>パスワード再設定</Typography>
-                            <TextField required id="outlined-required" label="メールアドレス" value={""}
-                                       onChange={() => null} error={false} helperText={null}/>
-                            <Button variant="contained" onClick={() => null}>再設定メールを送信する</Button>
+                            <TextField required id="outlined-required" label="メールアドレス" value={email}
+                                       onChange={onChangeEmail}/>
+                            <Button variant="contained" onClick={onClickSendEmailButton}
+                                    disabled={sendEmailButtonDisabled}>再設定メールを送信する</Button>
                         </Stack>
                     </CardContent>
                 </Card>
